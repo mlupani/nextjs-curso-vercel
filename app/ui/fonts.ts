b5db49a5d6b465116d64da5f415afae77703077e
Miguel Lupani <mlupani2@gmail.com>
2023-11-09 17:55:25 -0300

    cambios

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Montserrat, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const montserrat = Montserrat({ subsets: ['latin'], weight: ['400'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400'] });
